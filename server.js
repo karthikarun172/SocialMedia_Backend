5c1ac707dbd9e514ffcaf58dc164041ba3cfac84
karthikarun172 <57855758+karthikarun172@users.noreply.github.com>
2022-10-12 15:18:38 +0530

    connected to atlas

--- server.js
+++ server.js
@@ -14,25 +14,14 @@ const authRoute = require('./Routes/auth')
 const postRoute = require('./Routes/PostRoute')
 
 mongoose
-  .connect('mongodb://localhost/SocalMedia_App')
+  .connect(
+    'mongodb+srv://arunkarthik:7GWkWQNgZ3FBn2eC@cluster0.zwechjs.mongodb.net/?retryWrites=true&w=majorityp',
+  )
   .then(() => console.log('Connected to Database'))
   .catch((er) => console.log('Could not connect', er))
 
 app.use(cors())
 
-app.use('/', (req, res, next) => {
-  res.send('hello world with ssl')
-})
-
-const sslServer = https.createServer(
-  {
-    key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
-    cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
-  },
-  app,
-)
-
-sslServer.listen(3443, () => console.log('secure server on port 3443'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(helmet())
